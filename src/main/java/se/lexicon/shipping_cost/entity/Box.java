9dde9f015a4820f7dfd0a0fd936a0bf347ddaaef
aman adinlid <95pinkpanda@gmail.com>
2021-04-17 00:25:12 +0200

    ##created box class and implement it##

---
+++ src/main/java/se/lexicon/shipping_cost/entity/Box.java
@@ -0,0 +1,47 @@
+package se.lexicon.shipping_cost.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Box {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIBGenerator")
+    @Column(nullable = false, updatable = false)
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String country;
+
+    @Column(nullable = false, length = 50)
+    private String type;
+
+    @Column(nullable = false)
+    private double cost;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(nullable = false, length = 50)
+    private String weightType;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "TimesTamp default Current_TimeStamp")
+    private LocalDateTime createDate;
+
+    @CreationTimestamp()
+    @Column(nullable = false, columnDefinition = "tinyint(1) default 1")
+    private boolean status;
+}
