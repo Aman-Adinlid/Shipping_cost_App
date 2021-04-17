83fb8609b88276ec8e779c601e358fc0685eec8c
aman adinlid <95pinkpanda@gmail.com>
2021-04-17 02:57:22 +0200

    ##done box list page##

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.lexicon.shipping_cost.entity.Box;
 import se.lexicon.shipping_cost.repository.BoxRepository;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Controller
 @RequestMapping("/shipping")
@@ -34,16 +38,24 @@ public class ShippingController {
     }
 
     @GetMapping("/")
-    public String getAll() {
+    public String getAll(Model model) {
+        List<Box>boxes = new ArrayList<>();
+        Iterator<Box> iterator = boxRepository.findAll().iterator();
+        iterator.forEachRemaining(boxes::add);
+        model.addAttribute("boxes",boxes);
         return "showBoxList";
     }
 
     @PostMapping("/add")
-    public String addBox(@ModelAttribute("box") @Valid Box box, BindingResult bindingResult) {
+    public String addBox(@ModelAttribute("box") @Valid Box box, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         System.out.println("box = " + box);
         if (bindingResult.hasErrors()) {
             return "addBoxForm";
         }
+        boxRepository.save(box);//send data to db
+        redirectAttributes.addFlashAttribute("message","Operations is done");
+        redirectAttributes.addFlashAttribute("alertClass","alert-successes");
+
         return "redirect:/shipping/";
 
     }
